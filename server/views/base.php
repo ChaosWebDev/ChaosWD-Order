1166ce677e8aede0947e3bf9d7c4d8441a7db230
jpgerber1986 <jordangerber@gmail.com>
2023-05-25 13:42:10 -0600

    v0.1.0

--- server/views/base.php
+++ server/views/base.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ChaosWD\Order Framework</title>
     <link rel="stylesheet" href="/assets/styles.css">
     <link rel="manifest" href="/assets/favicons/manifest.json">
     <link rel="shortcut icon" href="/assets/favicons/favicon.ico" type="image/x-icon">
     <!--
     <script src='/assets/scripts/___.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     -->
 </head>
 
